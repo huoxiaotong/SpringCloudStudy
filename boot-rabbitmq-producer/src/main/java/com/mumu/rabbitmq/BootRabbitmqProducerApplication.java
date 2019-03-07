8e1e8b28c1396b72e25c5f8c6ea8b86329ce3eb1
huoxiaotong <690893206@qq.com>
2019-03-07 21:35:06 +0800

    feign 集成

---
+++ boot-rabbitmq-producer/src/main/java/com/mumu/rabbitmq/BootRabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.mumu.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRabbitmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRabbitmqProducerApplication.class, args);
+    }
+
+}
